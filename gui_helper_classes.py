f10014d3f9e895bdc8762c221d9f34c3a40ca174
Elias Ankerhold <elias.ankerhold@gmail.com>
2022-05-27 15:16:24 +0300

    added chart, only needs fine tuning

--- gui_helper_classes.py
+++ gui_helper_classes.py
@@ -1,7 +1,10 @@
 from PyQt5.QtWidgets import (
-    QVBoxLayout, QHBoxLayout,
-    QWidget, QDialog, QDialogButtonBox, QLabel, QGraphicsEllipseItem, QFileDialog
+    QVBoxLayout,
+    QDialog, QDialogButtonBox, QLabel, QGraphicsEllipseItem, QFileDialog
 )
+import pyqtgraph as pg
+from pyqtgraph import PlotWidget
+import numpy as np
 
 from connector import LocalDatabase
 
@@ -11,13 +14,93 @@ def form_extractor(form, field):
     return form.itemAt(x, y).widget().text()
 
 
-class PieChart(QWidget):
-    def __init__(self, data: list[float], labels: list[str]):
+nutrient_labels = ['Energy', 'Fat', 'Sat. Fat', 'Fiber', 'Carbs', 'Sugar', 'Protein', 'Salt']
+
+
+class PieChart(PlotWidget):
+    def __init__(self, data: list[float] = None, labels: list[str] = None):
         super().__init__()
+        self.setBackground('w')
+        self.setXRange(0, 1.1)
+        self.setYRange(0, 1.1)
+        self.setAspectLocked(True)
+        self.setMouseEnabled(x=False, y=False)
+        self.hideButtons()
+
+        plot = self.getPlotItem()
+
+        plot.hideAxis('bottom')
+        plot.hideAxis('left')
+
+        if data is None:
+            return
+
+        self.reduced_data = [data[1], data[3], data[4], data[6], data[7]]
+        self.reduced_labels = [labels[1], labels[3], labels[4], labels[6], labels[7]]
+        print(self.reduced_data)
+
         self.slices = []
-        for i, d in enumerate(data):
-            this_slice = QGraphicsEllipseItem()
-            self.slices.append(this_slice)
+        self.labels = []
+        self.spans = []
+        self.start_angles = []
+        self.colors = ['#af529f', '#ffa4b6', '#2a94b6', '#f9310e', '#1fb835']
+
+        full_circle = 360 * 16
+        start_angle = full_circle * 0.75
+        norm_fac = float(1 / sum(self.reduced_data))
+        for i, d in enumerate(self.reduced_data):
+            p_ellipse = QGraphicsEllipseItem(0, 0, 1, 1)
+            p_ellipse.setPen(pg.mkPen(255, 255, 255))
+            p_ellipse.setBrush(pg.mkBrush(self.colors[i]))
+
+            p_ellipse.setStartAngle(start_angle)
+            span = d * norm_fac * full_circle
+            p_ellipse.setSpanAngle(span)
+
+            center_angle_radian = (start_angle - full_circle * 0.75 + 0.5 * span) * 2 / full_circle * np.pi
+
+            radius = 0.4
+            if span < full_circle * 0.025:
+                radius = 0.55
+            text = pg.TextItem(self.reduced_labels[i], (0, 0, 0), anchor=(0, 0))
+            text.setPos(radius * np.sin(center_angle_radian) + 0.45, radius * np.cos(center_angle_radian) + 0.525)
+
+            self.addItem(p_ellipse)
+            self.slices.append(p_ellipse)
+            self.labels.append(text)
+            self.start_angles.append(start_angle)
+            self.spans.append(span)
+
+            start_angle += span
+
+        pair_inds = [0, 2]
+        self.add_colors = ['#af94ee', '#12cbff']
+        self.add_labels = ['Sat. Fat', 'Sugar']
+
+        for i, d in enumerate([data[2], data[5]]):
+            p_ellipse = QGraphicsEllipseItem(0, 0, 1, 1)
+            p_ellipse.setPen(pg.mkPen(255, 255, 255))
+            p_ellipse.setBrush(pg.mkBrush(self.add_colors[i]))
+            p_ellipse.setOpacity(0.6)
+
+            p_ellipse.setStartAngle(self.start_angles[pair_inds[i]])
+            span = d * norm_fac * full_circle
+            p_ellipse.setSpanAngle(span)
+
+            center_angle_radian = (self.start_angles[
+                                       pair_inds[i]] - full_circle * 0.75 + 0.5 * span) * 2 / full_circle * np.pi
+
+            radius = 0.4
+            if span < full_circle * 0.025:
+                radius = 0.55
+            text = pg.TextItem(self.add_labels[i], (0, 0, 0), anchor=(0, 0))
+            text.setPos(radius * np.sin(center_angle_radian) + 0.45, radius * np.cos(center_angle_radian) + 0.525)
+
+            self.addItem(p_ellipse)
+            self.labels.append(text)
+
+        for t in self.labels:
+            self.addItem(t)
 
 
 class RemoveDialog(QDialog):
