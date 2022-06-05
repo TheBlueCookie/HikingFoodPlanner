e8710da34fbf340631e901ca0c3b24917248bd1c
Elias Ankerhold <elias.ankerhold@gmail.com>
2022-06-05 16:57:14 +0300

    restructered variables and added functions - changed meal_plane from list[list] to list[dict], hard coding the number of meals per day to 4 - abandoned total cost and weight variables - added add_day, get day summary and get meal plan summary functions

--- trip_backend.py
+++ trip_backend.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass, field
-from food_backend import Meal, LocalDatabaseComponent
+from food_backend import Meal, LocalDatabaseComponent, MealType, n_nutrients
+from typing import Union
+
+import numpy as np
+import numpy.typing as npt
 
 
 @dataclass
@@ -8,35 +12,55 @@ class Trip(LocalDatabaseComponent):
 
     Args:
         name (str): Name of the trip.
-        duration (int): Initial duration in days.
-        meals_per_day (int): Standard number of planned meals per day."""
+        duration (int): Initial duration in days."""
 
-    name: str
-    duration: int
-    meals_per_day: int
-    meals: list[list[Meal]] = field(default_factory=[[]])
-    total_cost: float = 0
-    total_weight: float = 0
+    name: str = ''
+    duration: int = 0
+    meal_plan: list[dict[int, Union[Meal, None]]] = field(default_factory=list[dict])
 
     def __post_init__(self):
         super().__init__(name=self.name)
+        for i in range(self.duration):
+            self.add_day(init_mode=True)
 
-    def add_days(self, n_days: int):
-        """
-        Adds days to the trip.
+    def add_day(self, init_mode=False):
+        self.meal_plan.append({0: None, 1: None, 2: None, 3: None})
+        if not init_mode:
+            self.duration += 1
 
-        :param n_days: How many days to add.
-        """
-        self.duration += n_days
-        self.meals.append([] * n_days)
+    def set_meal_at_day(self, meal: Meal, day_ind: int, meal_type: MealType) -> bool:
+        if day_ind > self.duration - 1:
+            return False
 
-    def update_all_fields(self):
-        """
-        Updates all relevant fields.
-        """
-        self.total_cost = 0
-        self.total_weight = 0
-        for day in self.meals:
-            for meal in day:
-                self.total_cost += meal.cost
-                self.total_weight += meal.weight
+        self.meal_plan[day_ind][meal_type.code] = meal
+        return True
+
+    def get_day_summary(self, day_ind) -> (npt.NDArray, float, float, int):
+        if day_ind > self.duration - 1:
+            return False
+
+        nutrition = np.zeros(n_nutrients)
+        cost = 0
+        weight = 0
+        cooking_count = 0
+        for i, meal in enumerate(self.meal_plan[day_ind].values()):
+            nutrition = nutrition + meal.nutrition
+            cost += meal.cost
+            weight += meal.weight
+            cooking_count += int(meal.cooking)
+
+        return nutrition, cost, weight, cooking_count
+
+    def get_meal_plan_summary(self) -> (npt.NDArray, float, float, int):
+        nutrition = np.zeros(n_nutrients)
+        cost = 0
+        weight = 0
+        cooking_count = 0
+        for i, day in enumerate(self.meal_plan):
+            day_nut, day_cost, day_weight, day_cooking = self.get_day_summary(day_ind=i)
+            nutrition = nutrition + day_nut
+            cost += day_cost
+            weight += day_weight
+            cooking_count += day_cooking
+
+        return nutrition, cost, weight, cooking_count
