fa3f5348238e9f514b168832224510bb68e19685
Elias Ankerhold <elias.ankerhold@gmail.com>
2022-06-05 17:40:22 +0300

    added creation of Trip to be passed to MainWindow

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from gui_main_classes import Application, MainWindow
 from connector import LocalDatabase
 import sys
 
+from trip_backend import Trip
+
 if __name__ == '__main__':
     database = LocalDatabase()
+    trip = Trip()
     app = Application()
-    window = MainWindow(local_database=database)
+    window = MainWindow(local_database=database, trip=trip)
     window.show()
     sys.exit(app.exec_())
