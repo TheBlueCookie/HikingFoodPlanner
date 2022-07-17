489651f856c4b0d2734961a197144bab386188a2
Elias Ankerhold <elias.ankerhold@gmail.com>
2022-07-18 00:11:25 +0300

    started packaging

---
+++ PyHikingPlanner/README.md
@@ -0,0 +1,1 @@
+Python application for meal planning. Work in Progress.
---
+++ PyHikingPlanner/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='PyHikingPlanner',
+    version='0.0.1',
+    author='Elias Ankerhold',
+    author_email='elias.ankerhold@gmail.com',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/TheBlueCookie/HikingFoodPlanner.git',
+    license='GPL-3.0-or-later',
+    packages=['my_own_package\\gui', 'my_own_package\\main'],
+    install_requires=['numpy>=1.21.1', 'pandas>=1.3.1', 'PyQt5>=5.15.7', 'pyqtgraph>=0.12.4'],
+)
