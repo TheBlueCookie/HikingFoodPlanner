0727798dac2e7cfa41649704471de6e0ae088523
Elias Ankerhold <elias.ankerhold@gmail.com>
2022-07-18 09:46:53 +0300

    moved main.py

---
+++ PyHikingPlanner/main.py
@@ -0,0 +1,17 @@
+import sys
+
+from PyHikingPlanner.app.connector import LocalDatabase
+from PyHikingPlanner.backend.trip import Trip
+from PyHikingPlanner.gui.main_classes import Application, MainWindow
+
+
+def run():
+    if __name__ == '__main__':
+        database = LocalDatabase()
+        trip = Trip(CODE=0, name='', meal_types=database.meal_types)
+        app = Application()
+        window = MainWindow(local_database=database, trip=trip)
+        window.show()
+        sys.exit(app.exec_())
+
+run()
