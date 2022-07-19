0cc4620edc4096edd268dafdda39eddb9f788ee9
TheBlueCookie <elias.ankerhold@gmail.com>
2022-07-19 11:26:24 +0300

    added automatic directory preparation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from src.app.connector import LocalDatabase
 from src.backend.trip import Trip
@@ -6,14 +7,23 @@ from src.gui.main_classes import Application, MainWindow
 
 
 def run():
-    if __name__ == '__main__':
-        database = LocalDatabase()
-        trip = Trip(CODE=0, name='', meal_types=database.meal_types)
-        app = Application()
-        window = MainWindow(local_database=database, trip=trip)
-        window.show()
-        sys.exit(app.exec_())
+    database = LocalDatabase()
+    trip = Trip(CODE=0, name='', meal_types=database.meal_types)
+    app = Application()
+    window = MainWindow(local_database=database, trip=trip)
+    window.show()
+    sys.exit(app.exec_())
+
+def prepare_dirs():
+    for d in ['databases', 'trips', 'shopping_lists']:
+        if not os.path.isdir(f'.\\saves\\{d}'):
+            os.mkdir(f'.\\saves\\{d}')
+    
+    if not os.path.isdir(f'.\\config'):
+        os.mkdir(f'.\\mkdir')
 
 
 if __name__ == '__main__':
+    print('Application started...\n')
+    prepare_dirs()
     run()
